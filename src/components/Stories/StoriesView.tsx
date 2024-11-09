cc565c748ac731f9671c0b93496c045f0b0c6f21
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-11-09 10:58:19 -0300

    tiktok view 1

--- src/components/Stories/StoriesView.tsx
+++ src/components/Stories/StoriesView.tsx
@@ -1,8 +1,8 @@
 import { Photo } from "@/components/Stories/camera/useStockPhotoHook";
 import { PhotoThumbnail } from "@/components/Stories/PhotoThumbnail";
-import { Button, Flex, Grid, Text } from "@mantine/core";
+import { Button, Flex, Grid, Switch, Text } from "@mantine/core";
 import { ScrollArea, Avatar, Box } from "@mantine/core";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useScrollSnap from "react-use-scroll-snap";
 
 export const StoriesView = ({
@@ -14,41 +14,51 @@ export const StoriesView = ({
 }) => {
   const scrollRef = useRef(null);
 
-  const { goto, state } = useScrollSnap({ ref: scrollRef, duration: 100 });
+  const { goto: goTo, state } = useScrollSnap({
+    ref: scrollRef,
+    duration: 100,
+  });
+
+  const [autoScroll, setAutoScroll] = useState(true);
+  const [dotIndex, setDotIndex] = useState(0);
+
+  // interval scrolling to the next photo every 1 seconds
+  useEffect(() => {
+    if (!autoScroll) return;
+    const interval = setInterval(() => {
+      goTo((state.current?.currentIndex || 0) + 1);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [state.current?.currentIndex, autoScroll]);
+
+  useEffect(() => {
+    setDotIndex(state.current?.currentIndex || 0);
+  }, [state]);
 
   return (
     <>
-      <div
+      <Flex
         style={{
           position: "fixed",
           zIndex: 1000,
           top: 10,
-          left: 0,
-          width: "100%",
+          right: 10,
           userSelect: "none",
-          pointerEvents: "none",
           display: "flex",
           flexDirection: "row",
-          justifyContent: "center",
+          justifyContent: "space-between",
           alignItems: "center",
         }}
       >
-        {/* dots */}
-        {photos.map((photo, index) => (
-          <div
-            key={index}
-            style={{
-              width: index === state.current?.currentIndex ? 10 : 5,
-              height: 2,
-              borderRadius: 5,
-              background: "white",
-              margin: 2,
-              opacity: 0.5,
-              transition: "opacity 0.2s",
-            }}
-          />
-        ))}
-      </div>
+        <Switch
+          label="Auto scroll"
+          checked={autoScroll}
+          onChange={() => setAutoScroll((prev) => !prev)}
+          color="yellow"
+          size="lg"
+          style={{ userSelect: "none", color: "white" }}
+        />
+      </Flex>
 
       <main ref={scrollRef} style={{ background: "black" }}>
         {photos.map((photo, index) => (
